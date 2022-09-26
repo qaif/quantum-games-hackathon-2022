599517e07b6e89845e204d3e159d8b776c3d9106
Handy Kurniawan <62504305+HandyKurniawan@users.noreply.github.com>
2022-09-26 22:20:34 +0300

    clean code

--- assets/classes/measurementbase.py
+++ assets/classes/measurementbase.py
@@ -2,22 +2,71 @@ import assets.constants as c
 import pygame
 import random
 
-class MovingObject(pygame.sprite.Sprite):
+class BaseObject(pygame.sprite.Sprite):
+    """
+    Class for Base Object.
+    """
     def __init__(self):
         super().__init__()
 
-    x_change = -1
-    y_change = 0
-
     def update(self, surface: pygame.Surface):
+        """
+            To keep the object appear on the screen
+        """
         surface.blit(self.image, self.rect)
 
+
+
+class StationaryObject(BaseObject):
+    """
+        Class for Stationary Object
+    """
+    def __init__(self):
+        super().__init__()
+
+class MovingObject(BaseObject):
+    """
+    Class for Moving Object
+    """
+    def __init__(self):
+        super().__init__()
+
+
+    x_change = 0
+    "how many pixels object move on the horizontal line"
+
+    y_change = 0
+    "how many pixels move on the vertical line"
+
     def move(self):
+        """
+        move to object based on x_change and y_change values
+        :return: null
+        """
         self.rect.x += self.x_change
         self.rect.y += self.y_change
 
+    def set_x_change(self, x: int):
+        """
+        set the value of x_change
+        :param x: number of pixels the object move horizontally ( negative: move left, positive: move right)
+        :return: null
+        """
+        self.x_change = x
+
+    def set_y_change(self, y: int):
+        """
+        set the value of y_change
+        :param y: number of pixels the object move vertically ( negative: move up, positive: move down)
+        :return: null
+        """
+        self.y_change = x
+
 
 class MeasurementBase(MovingObject):
+    """
+    Class for Measurement base in Phase 1
+    """
     measurements = [(pygame.image.load("assets/images/letter-z.png"), pygame.K_z, 278),
                     (pygame.image.load("assets/images/letter-x.png"), pygame.K_x, 198)]
 
@@ -28,18 +77,23 @@ class MeasurementBase(MovingObject):
         y = 0
         self.x_change = -1 * random.randint(1, 2)
 
-        if type == 122:
+        # to get the type base on the given key, else take randomly
+        if type == pygame.K_z:
             self.image, self.key, y = self.measurements[0]
-        elif type == 120:
+        elif type == pygame.K_x:
             self.image, self.key, y = self.measurements[1]
         else:
             self.image, self.key, y = random.choice(self.measurements)
 
+        # set initialize position
         self.rect = self.image.get_rect(topleft=(x, y))
 
 
 
 class BitBase(MovingObject):
+    """
+        Class for Bit base in Phase 1
+    """
     bits = [(pygame.image.load("assets/images/number-1.png"), pygame.K_1, 117),
             (pygame.image.load("assets/images/number-0.png"), pygame.K_0, 44)]
 
@@ -50,9 +104,10 @@ class BitBase(MovingObject):
         y = 0
         self.x_change = -1 * random.randint(1, 2)
 
-        if type == 49:
+        # to get the type base on the given key, else take randomly
+        if type == pygame.K_1:
             self.image, self.key, y = self.bits[0]
-        elif type == 48:
+        elif type == pygame.K_0:
             self.image, self.key, y = self.bits[1]
         else:
             self.image, self.key, y = random.choice(self.bits)
--- main.py
+++ main.py
@@ -14,15 +14,18 @@ window = pygame.display.set_mode((1024, 768))
 
 pygame.display.set_caption("Verona 2049")
 
+# creating a group of Sprite, this is like an array of sprite (object of image)
 measurements = pygame.sprite.Group()
 retrieved_measurements = pygame.sprite.Group()
 bits = pygame.sprite.Group()
 retrieved_bits = pygame.sprite.Group()
 
+# user defined function
 measurement_event = pygame.USEREVENT + 1
 bit_event = pygame.USEREVENT + 2
 move_event = pygame.USEREVENT + 3
 
+# timer for user defined function
 pygame.time.set_timer(measurement_event, 3000) # 2000 milliseconds = 2 seconds
 pygame.time.set_timer(bit_event, 2500) # 2000 milliseconds = 2 seconds
 pygame.time.set_timer(move_event, 5) # 2000 milliseconds = 2 seconds
@@ -46,45 +49,69 @@ def player(x,y):
     pass
 
 def score_display(x,y):
+    """
+    Display the score
+    :param x: x position on the screen
+    :param y: y position on the screen
+    :return: null
+    """
     point_scored = point_font.render("Score : " + str(point), True, (255, 255, 255))
     window.blit(point_scored, (x, y))
 
 def missing_display(x,y):
+    """
+    Display the missing point
+    :param x: x position on the screen
+    :param y: y position on the screen
+    :return: null
+    """
     point_missing = missing_font.render("Miss : " + str(missing), True, (255, 255, 255))
     window.blit(point_missing, (x, y))
 
 def get_measurement(key: int):
+    """
+    get the measurement base and put into the table
+    :param key: key press (X, Z)
+    :return: boolean
+    """
     for l in measurements:
         if l.rect.x >= 30 and l.rect.x <= 90 and l.key == key:
 
+            # creating new object for the static measurement
             retrieved_measurement = MeasurementBase(key)
-
             retrieved_measurement.rect = retrieved_measurement.image.get_rect(topleft=(50 + (40 * len(retrieved_measurements)), 597))
-            #retrieved_measurement.y = 597
-            #retrieved_measurement.x = 50 + (40 * len(retrieved_measurements))
             retrieved_measurements.add(retrieved_measurement)
 
+            # delete the sprite object
             l.kill()
             return True
 
     return False
 
 def get_bit(key: int):
+    """
+        get the bit base and put into the table
+        :param key: key press (0, 1)
+        :return: boolean
+        """
     for l in bits:
         if l.rect.x >= 30 and l.rect.x <= 90 and l.key == key:
-
+            # creating new object for the static bit
             retrieved_bit = BitBase(key)
             retrieved_bit.rect = retrieved_bit.image.get_rect(topleft=(50 + (40 * len(retrieved_bits)), 437))
-            #retrieved_bit.y = 437
-            #retrieved_bit.x = 50 + (40 * len(retrieved_bits))
             retrieved_bits.add(retrieved_bit)
 
+            # delete the sprite object
             l.kill()
             return True
 
     return False
 
 def is_measurement_miss():
+    """
+    to detect if the user miss the measurement base
+    :return: boolean
+    """
     for l in measurements:
         if l.rect.x <= 0:
             l.kill()
@@ -93,6 +120,10 @@ def is_measurement_miss():
     return False
 
 def is_bit_miss():
+    """
+        to detect if the user miss the bit base
+        :return: boolean
+        """
     for l in bits:
         if l.rect.x <= 0:
             l.kill()
@@ -101,25 +132,27 @@ def is_bit_miss():
     return False
 
 
+
 run = True
 while run:
 
+    # to show the background
     window.blit(background, (0, 0))
 
     last = pygame.time.get_ticks()
 
-
+    # getting all event happens on the game (mouse hover, keyboard press, user defined function)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: # for quiting the game
             pygame.quit()
             sys.exit()
-        elif event.type == measurement_event and len(retrieved_measurements) <= 10:
-            # spawn new letter
+        elif event.type == measurement_event and len(retrieved_measurements) <= 10: # to keep spawning the measurement base until reach x values
+            # spawn new object
             measurements.add(MeasurementBase())
             total_measurement += 1
 
-        elif event.type == bit_event  and len(retrieved_bits) <= 10:
-            # spawn new letter
+        elif event.type == bit_event  and len(retrieved_bits) <= 10: # to keep spawning the bit base until reach x values
+            # spawn new object
             bits.add(BitBase())
             total_bit += 1
 
@@ -134,18 +167,17 @@ while run:
             if get_bit(event.key) or get_measurement(event.key):
                 point += 1
 
-
-    measurements.draw(window)
-    bits.draw(window)
-    retrieved_measurements.draw(window)
-    retrieved_bits.draw(window)
-
     if is_measurement_miss():
         missing += 1
 
     if is_bit_miss():
         missing += 1
 
+    # to keep the object refreshing on the screen
+    measurements.draw(window)
+    bits.draw(window)
+    retrieved_measurements.draw(window)
+    retrieved_bits.draw(window)
     score_display(pointx, pointy)
     missing_display(missingx, missingy)
     pygame.display.update()
