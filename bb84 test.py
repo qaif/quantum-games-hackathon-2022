a3783ac617aae214edeea76d166942d3d76753a3
tylergcowan <113568149+tylergcowan@users.noreply.github.com>
2022-09-27 19:02:58 -0400

    bb84 defs, encryption class, music files
    - adds encryption class
    - 1 music file and folder for it
    - more functions for use in bb84 encryption protocol

--- bb84 test.py
+++ bb84 test.py
@@ -10,9 +10,12 @@ from numpy.random import randint
 import numpy as np
 import itertools
 from qiskit.providers.aer import QasmSimulator
-import operator
 num_qubits = 32
 
+# alice/romeo is picking bits and basis randomly
+# this needs to happen before phase 1 starts, so that minigame is correctly
+# populated
+# basis pf 1 or 0 can be converted to x or z later in the minigame
 alice_basis = np.random.randint(2, size=num_qubits)
 alice_state = np.random.randint(2, size=num_qubits)
 bob_basis = np.random.randint(2, size=num_qubits)
@@ -21,6 +24,73 @@ bob_basis = np.random.randint(2, size=num_qubits)
 print (f"Alice's State:\t", np.array2string(alice_state))
 print (f"Alice's Bases:\t", np.array2string(alice_basis))
 print (f"Bob's Bases:\t",np.array2string(bob_basis))
+
+
+#The function encode_message below, creates a list of QuantumCircuits,
+# # each representing a single qubit in Alice's message:
+#  https://qiskit.org/textbook/ch-algorithms/quantum-key-distribution.html
+def encode_message(bits, bases):
+    message = []
+    for i in range(n):
+        qc = QuantumCircuit(1,1)
+        if bases[i] == 0: # Prepare qubit in Z-basis
+            if bits[i] == 0:
+                pass
+            else:
+                qc.x(0)
+        else: # Prepare qubit in X-basis
+            if bits[i] == 0:
+                qc.h(0)
+            else:
+                qc.x(0)
+                qc.h(0)
+        qc.barrier()
+        message.append(qc)
+    return message
+
+# applies corresponding measurement, simulating result of each qubit upon measurement
+# results are stored in bob's rsults
+# this is also used to do interception by eve, which will be decided probabilistically
+def measure_message(message, bases):
+    backend = Aer.get_backend('aer_simulator')
+    measurements = []
+    for q in range(n):
+        if bases[q] == 0: # measuring in Z-basis
+            message[q].measure(0,0)
+        if bases[q] == 1: # measuring in X-basis
+            message[q].h(0)
+            message[q].measure(0,0)
+        aer_sim = Aer.get_backend('aer_simulator')
+        qobj = assemble(message[q], shots=1, memory=True)
+        result = aer_sim.run(qobj).result()
+        measured_bit = int(result.get_memory()[0])
+        measurements.append(measured_bit)
+    return measurements
+
+# sifting stage.
+def remove_garbage(a_bases, b_bases, bits):
+    good_bits = []
+    for q in range(n):
+        if a_bases[q] == b_bases[q]:
+            # If both used the same basis, add
+            # this to the list of 'good' bits
+            good_bits.append(bits[q])
+    return good_bits
+
+
+# phase 4; comparing random select of bits in their keys
+# make sure the sampling is random!
+def sample_bits(bits, selection):
+    sample = []
+    for i in selection:
+        # use np.mod to make sure the
+        # bit we sample is always in
+        # the list range
+        i = np.mod(i, len(bits))
+        # pop(i) removes the element of the
+        # list at index 'i'
+        sample.append(bits.pop(i))
+    return sample
 
 def bb84_circuit(state, basis, measurement_basis):
     '''
@@ -111,7 +181,4 @@ c=cipher_decryption(a,encryption_key)
 
 
 exit()
-
-
-
 
