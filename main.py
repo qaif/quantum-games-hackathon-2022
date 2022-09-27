66873027c7637c94c4dd88cfb9fc4bd4b8efbc0e
tylergcowan <113568149+tylergcowan@users.noreply.github.com>
2022-09-26 21:09:29 -0400

    key compare minigame pt 1
    first step in building the key comparison minigame

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pygame
 from pygame import mixer
 from typing import List
 from assets.scenes.games_1 import Games_1
+from assets.scenes.games_2 import Games_2
 
 # init the pygame
 pygame.init()
@@ -15,14 +16,12 @@ window = pygame.display.set_mode((1024, 768))
 # set the window caption
 pygame.display.set_caption("Verona 2049")
 
-#last = pygame.time.get_ticks() #forget what this is for
-
 # declare the game phase 1
 g1 = Games_1(pygame)
+g2 = Games_2(pygame)
 
 #### HARD CODE #####
-phase = 1
-
+phase = 2
 
 run = True
 while run:
@@ -33,23 +32,19 @@ while run:
 
         # call all the event related to game 1
         g1.call_event(window)
+
     elif (phase==2):
 
-        window.blit(background, (0, 0))
-        last = pygame.time.get_ticks()
+        # update background for new phase
+        window.blit(g2.background, (0, 0))
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        # hardcoding the info that will be fed into the next phase (even though this is rly phase 4 not 2!)
+        bits1 = [1, 0, 1, 1, 1, 1, 0, 1, 0, 0]
+        bits2 = [1, 0, 1, 0, 1, 0, 0, 1, 1, 0]
 
+        # call all the event related to game 2
+        g2.call_event(window)#,bits_compared)
 
-
-
-
-        pygame.display.update()
-
-        pass
 
     # update the display for pygame
     pygame.display.update()
