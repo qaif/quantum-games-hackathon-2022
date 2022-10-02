9666f51a2edd22247011dcde64dcd7a70bc18df8
tylergcowan <113568149+tylergcowan@users.noreply.github.com>
2022-10-01 23:22:37 -0400

    update story 2

--- assets/scenes/story_2.py
+++ assets/scenes/story_2.py
@@ -19,9 +19,10 @@ class Story_2(Story):
         self.story_index = 0
 
         self.story_text.append("Juliet: Welcome to the Capulet ball, all the noise in here gives us privacy.")
-        self.story_text.append("Eve: Yes, but that noise can compromise the qubits. Measure them, quickly.")
+        self.story_text.append("Eve: Yes, but this chaos can decohere the qubits! Measure them, quickly.")
         self.story_text.append("Juliet: I’ll randomly pick the X or Z basis during measurement.")
         self.story_text.append("Eve: Good luck measuring them with all the commotion in here.")
+        self.story_text.append("Eve: Qubits are slippery things, and very sensitive to disruptions.")
 
         self.y = 350
         self.font = 20
@@ -37,7 +38,8 @@ class Story_2_5(Story):
         self.story_text = []
         self.story_index = 0
 
-        self.story_text.append("Eve: Great work. The qubits were all measured and stored as 1s and 0s.")
+        self.story_text.append("Eve: The qubits were all measured and stored as classical 1s and 0s.")
+        self.story_text.append("Eve: I hope the qubits didn't decohere too much...I don't wanna be blamed for anything.")
         self.story_text.append("Eve: You'll need those measurements for the next stage in creating the key.")
 
         self.y = 350
