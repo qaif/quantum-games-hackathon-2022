fa1d5f433f614958837fd2686df8101966b90626
tylergcowan <113568149+tylergcowan@users.noreply.github.com>
2022-10-01 23:05:01 -0400

    more gameover text

--- assets/scenes/gameover.py
+++ assets/scenes/gameover.py
@@ -14,7 +14,8 @@ class GameOver(Story):
         self.story_text = []
         self.story_index = 0
 
-        self.story_text.append("GAME OVER. You've run out of time or lives!")
+        self.story_text.append("GAME OVER. You've run out of time, lives, or your key has no bits left to sample.")
+        self.story_text.append("The length of the key BB84 produces is not deterministic!")
         self.story_text.append("These violent delights have violent ends...")
         self.story_text.append("Your total score is: ")
         self.story_text.append(globals.total_score)
